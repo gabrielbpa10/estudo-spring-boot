6883a2663e971a771a65d75ff5a2c0b5f4cd1264
Gabriel Braga Paiva Araujo <951548246@M65069409.local>
2021-06-04 16:31:51 -0300

    continuando estudo spring

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,27 +1,26 @@
 package br.com.alura.forum.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import br.com.alura.forum.dto.TopicoDTO;
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
 
 import java.util.Arrays;
 import java.util.List;
 
-@Controller
+@RestController
 public class TopicosController {
     
     @RequestMapping("/topicos")
-    @ResponseBody
-    public List<Topico> getListTopicos(){
+    public List<TopicoDTO> getListTopicos(){
         Topico topico = new Topico(
             "Como instalar Spring Boot na minha máquina",
             "Não consigo instalar o framework na minha máquina", 
             new Curso("Spring Boot (Java)","Java")
             );
         
-            return Arrays.asList(topico);
+            return TopicoDTO.converter(Arrays.asList(topico));
     }
 }
