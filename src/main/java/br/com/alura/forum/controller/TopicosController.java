63cc2c46347ff302541ca0035649d6924ac1e86f
gabrieltester <gabrielbpa10@gmail.com>
2021-06-21 11:22:26 -0300

    Colocando Put mapping

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -7,11 +7,13 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import br.com.alura.forum.dto.AtualizacaoTopicoForm;
 import br.com.alura.forum.dto.TopicoDTO;
 import br.com.alura.forum.dto.TopicoDetalhesDTO;
 import br.com.alura.forum.dto.TopicoForm;
@@ -72,4 +74,11 @@ public class TopicosController {
         return ResponseEntity.ok().build();
     }
 
+    @PutMapping("/{id}")
+    @Transactional
+    public ResponseEntity<TopicoDTO> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form){
+        Topico topico = form.atualizar(id,topicoRepository);
+
+        return ResponseEntity.ok(new TopicoDTO(topico));
+    }
 }
